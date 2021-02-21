3efe1c890e68565ad3a29655aa947a2abab699d0
LettaSav <lettawetta@icloud.com>
2021-02-21 20:42:46 +0200

    Updates

---
+++ precache-manifest.e0e98be918707279a9f9c1840751fac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e938d0c544008858e017",
+    "url": "/goit-react-hw-01-components/static/css/main.38664507.chunk.css"
+  },
+  {
+    "revision": "e938d0c544008858e017",
+    "url": "/goit-react-hw-01-components/static/js/main.63a3bb05.chunk.js"
+  },
+  {
+    "revision": "b0f8f2e436fc577230d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.681d89f7.js"
+  },
+  {
+    "revision": "14cc0121a7d532faacc1",
+    "url": "/goit-react-hw-01-components/static/js/2.63de9bce.chunk.js"
+  },
+  {
+    "revision": "149f4f7af3e57639f77178579a610230",
+    "url": "/goit-react-hw-01-components/index.html"
+  }
+];
